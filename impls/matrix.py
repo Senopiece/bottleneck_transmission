9af9cf391d821b8f8d686b8431257bfb1872b222
Vitaly <shichiromail@gmail.com>
2025-11-10 13:38:00 +0300

    mv todos

--- impls/matrix.py
+++ impls/matrix.py
@@ -173,11 +173,6 @@ if __name__ == "__main__":
     else:
         print("\n⚠️ Did not recover within limit.")
 
-# TODO: try fat matrix
-# TODO: try matrix with a little known shape to coop with d of not exact count_fullrank_matrices(n)
-# TODO: play with the zero injection strategy
-# TODO: play with single cycle matrices
-
 
 def producer_constructor(index: int, n: int, d: int) -> Producer:
     assert d == count_fullrank_matrices(n)
