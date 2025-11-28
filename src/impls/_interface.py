2bbdca7fc86fd61382946d39417d0c17997df92a
Vitaly <shichiromail@gmail.com>
2025-11-28 19:23:39 +0300

    add simplest impl

--- src/impls/_interface.py
+++ src/impls/_interface.py
@@ -8,6 +8,26 @@ class Producer:
         """returns ndarray of shape (N,)"""
 
 
+class GeneratorProducer(Producer):
+    """
+    Wrap any Python generator so it behaves like a Producer:
+        producer.generate() -> next value of the generator
+    """
+
+    def __init__(self, generator):
+        """
+        Parameters
+        ----------
+        generator : generator or iterable
+            A Python generator object or anything implementing __next__().
+        """
+        self._gen = generator
+
+    def generate(self):
+        """Return the next item from the wrapped generator."""
+        return next(self._gen)
+
+
 class Recoverer(ABC):
     @abstractmethod
     def feed(self, data: np.ndarray | None) -> int | None:
